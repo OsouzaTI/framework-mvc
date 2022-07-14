f3cae7d71b8007f1a8e6e95fc698d0d289eef559
ozeias souza <ozeias.souza@ufrr.br>
2022-07-14 13:10:03 -0400

    .

--- App/Controller.php
+++ App/Controller.php
@@ -11,14 +11,10 @@ class Controller {
     public $component;
     public $assets;
     public $html;
-    public $mongo;
     public function __construct() {
         $this->component = new Component();
         $this->assets = new Assets();
         $this->html = new Html();
-        $this->mongo = new MongoDB('db_jornal');
-        // $this->mongo->connetion('mongodb://root:pass@mongo:27017'); 
-        $this->mongo->connetion('mongodb+srv://root:pass@cluster0.e4ljd0u.mongodb.net/?retryWrites=true&w=majority'); 
     }
     public function view($view, $data = []) {
         $render = function() use($view, $data) {
